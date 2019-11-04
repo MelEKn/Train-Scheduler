f5bcafb53293faa8bb8c128da15d48d5edab156c
Melissa Knapp <melknapp@gmail.com>
2019-11-03 22:24:24 -0500

    Created bootstrap styling for main html site; submit form for train name, dest, time, and freq works; still need to get the time variable to work using Moment.js

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -8,11 +8,25 @@ var firebaseConfig = {
     storageBucket: "mel-s-app.appspot.com",
     messagingSenderId: "167127100574",
     appId: "1:167127100574:web:942b9574771087f1ea1f8f"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  
-  
-  // Assign the reference to the database to a variable named 'database'
-  
-  var database = firebase.database();
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+// Assign the reference to the database to a variable named 'database'
+
+var database = firebase.database();
+
+// Button for adding Trains
+$("#add-train-btn").on("click", function (event) {
+    event.preventDefault();
+    var trainName = $("#train-name-input").val().trim();
+    var dest = $("#dest-input").val().trim();
+    var firstTrainTime = moment($("#first-time-input").val().trim(), "HH:mm").format("X");
+    var freq = $("#freq-input").val().trim();
+
+    console.log("trainName is " + trainName);
+    console.log("dest is " + dest);
+    console.log("firstTrainTime is " + firstTrainTime);
+    console.log("freq is " + freq);
+});
